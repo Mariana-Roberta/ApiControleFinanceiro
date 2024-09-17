652fd68fa9df13d577056e1c07c91e1dbe9fb46d
Mariana Roberta <marianaroberta.240@gmail.com>
2024-09-17 20:19:30 -0300

    primeiro commit

---
+++ src/main/java/com/grupoMarianaOttony/ApiControleFinanceiro/ApiControleFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.grupoMarianaOttony.ApiControleFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupoMarianaOttony/ApiControleFinanceiro/ApiControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoMarianaOttony.ApiControleFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
