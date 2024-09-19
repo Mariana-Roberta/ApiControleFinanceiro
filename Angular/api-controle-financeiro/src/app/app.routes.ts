697380c5b8fb803352416dd0c7afa8ca7563ee6e
Mariana Roberta <marianaroberta.240@gmail.com>
2024-09-18 21:06:16 -0300

    Formulário e Listagem de Pessoas

--- Angular/api-controle-financeiro/src/app/app.routes.ts
+++ Angular/api-controle-financeiro/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PessoaFormularioComponent } from './pessoa/pessoa-formulario/pessoa-formulario.component';
+import { PessoaListagemComponent } from './pessoa/pessoa-listagem/pessoa-listagem.component';
 
 export const routes: Routes = [
-  { path: 'pessoa/pessoa-formulario', component: PessoaFormularioComponent }
+  { path: 'pessoa/pessoa-formulario', component: PessoaFormularioComponent },
+  { path: 'pessoa/pessoa-listagem', component: PessoaListagemComponent }
 ];
--- Angular/api-controle-financeiro/src/app/services/pessoa/pessoa-formulario.service.ts
+++ Angular/api-controle-financeiro/src/app/services/pessoa/pessoa-formulario.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@angular/core';
-import {Pessoa} from "../model/form";
+import {Pessoa} from "../../model/pessoa";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaFormularioService {
 
-  pessoasForm: Pessoa[] = [];
+  // Lista para armazenar objetos do tipo Pessoa
+    pessoasList: Pessoa[] = [];
 
     constructor() { }
 
-    addPesssoa(form: Pessoa) {
-      form.id = this.pessoasForm.length > 0 ? this.pessoasForm.length + 1 : 1;
-      console.log(form);
-      this.pessoasForm.push(form);
+    // Método para adicionar uma nova pessoa à lista
+    addPesssoa(pessoa: Pessoa) {
+      // Define um ID para a nova pessoa baseado no tamanho da lista
+      pessoa.id = this.pessoasList.length > 0 ? this.pessoasList.length + 1 : 1;
+      console.log(pessoa);
+      this.pessoasList.push(pessoa); // Adiciona a pessoa à lista
     }
 
+    // Método para obter a lista de todas as pessoas
     getPessoas(): Pessoa[] {
-      return this.pessoasForm;
+      return this.pessoasList; // Retorna a lista de pessoas
     }
 
+    // Método para obter uma pessoa pelo ID
     getById(id: number | null): Pessoa {
-      return <Pessoa>this.pessoasForm.find(pessoa => pessoa.id === id);
+      // Busca a pessoa na lista pelo ID e retorna
+      return <Pessoa>this.pessoasList.find(pessoa => pessoa.id === id);
     }
 }
