570740f76e6825e6b138bb83a1a1b300d58591a8
Paulo Vitor <64035529+psicopop@users.noreply.github.com>
2024-09-24 17:21:01 -0300

    Formularios

--- Angular/api-controle-financeiro/src/app/meta/meta-form/meta-form.component.ts
+++ Angular/api-controle-financeiro/src/app/meta/meta-form/meta-form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {Meta} from "../../model/Meta"
+import {Form, FormsModule} from "@angular/forms";
+import {FloatLabelModule} from "primeng/floatlabel";
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+
 
 @Component({
   selector: 'app-meta-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FloatLabelModule,
+    ButtonModule,
+    InputTextModule,
+    FormsModule
+  ],
   templateUrl: './meta-form.component.html',
   styleUrl: './meta-form.component.css'
 })
 export class MetaFormComponent {
+
+  meta : Meta = {
+    id: 0,
+    
+  }
 
 }
