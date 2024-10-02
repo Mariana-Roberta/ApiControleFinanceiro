52e04a9ad51723bee4d96c0b493fd02c8d159911
Paulo Vitor <dr.pvrocha@gmail.com>
2024-10-01 21:28:09 -0300

    Atualizando a meta-list

--- Angular/api-controle-financeiro/src/app/meta/meta-list/meta-list.component.ts
+++ Angular/api-controle-financeiro/src/app/meta/meta-list/meta-list.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Meta } from  "../../model/meta";
 import { Button } from 'primeng/button';
 import { TableModule } from 'primeng/table';
-import {metaHttp}
+import { MetaHttpService } from '../../services/meta/meta-http.service';
+import { NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
 
 @Component({
   selector: 'app-meta-list',
@@ -16,14 +19,26 @@ import {metaHttp}
 })
 export class MetaListComponent implements OnInit {
   
-  metas: Meta
+  metas: Meta[] = []
+
+  constructor(private metaHttpService : MetaHttpService, private router : Router ){}
+
   
   
   
   
   
   ngOnInit() {
-    
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe(() => {
+      const navigation = this.router.getCurrentNavigation();
+      if(navigation?.extras.state){
+        
+      }
+    }
+
+    )
   }
 
 
